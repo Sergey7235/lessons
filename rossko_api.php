76e553a87f75cd77dd200243cf69aea498ca466f
Sergey7235 <yshkoff-serg@yandex.com>
2019-11-23 22:15:30 +0500

    rossko_api

--- rossko_api.php
+++ rossko_api.php
@@ -1,18 +1,64 @@
 <?php 
 
 
-$aHTTP['http']['header'] =  "User-Agent: PHP-SOAP/5.5.11\r\n";
+// $aHTTP['http']['header'] =  "User-Agent: PHP-SOAP/5.5.11\r\n";
 
-$aHTTP['http']['header'].= "username: sergey.yushkov@rossko.ru\r\n"."password: XXXXX\r\n";
+// $aHTTP['http']['header'].= "username: nikolayporohin@gmail.com\r\n"."password: XXXXX\r\n";
 
-$context = stream_context_create($aHTTP);
+// $context = stream_context_create($aHTTP);
+
+
+
+
+
+// $connect = array(
+//     'wsdl'    => 'http://api.rossko.ru/service/GetSearch',
+//     'options' => array(
+//         'connection_timeout' => 1,
+//         'trace' => true
+//     )
+// );
+
+// $param = array(
+//     'KEY1' => 'bb863005b6468e8ceffec6febeee4c84',
+//     'KEY2' => '614ef5b7b2bca40dd231c4dc2c79283d',
+//     'TEXT' => '333114'
+// );
+
+// $query  = new SoapClient($connect['wsdl'], $connect['options']);
+// $result = $query->GetSearch($param);
+
+// print_r($result);
+
+
+
+
+
+
+// $connect = array(
+//     'wsdl'    => 'http://api.rossko.ru/service/GetCheckoutDetails',
+//     'options' => array(
+//         'connection_timeout' => 1,
+//         'trace' => true
+//     )
+// );
+
+// $param = array(
+//     'KEY1' => 'bb863005b6468e8ceffec6febeee4c84',
+//     'KEY2' => '614ef5b7b2bca40dd231c4dc2c79283d'
+// );
+
+// $query  = new SoapClient($connect['wsdl'], $connect['options']);
+// $result = $query->GetCheckoutDetails($param);
+
+// print_r($result);
 
 
 
 
 
 $connect = array(
-    'wsdl'    => 'http://api.rossko.ru/service/GetSearch',
+    'wsdl'    => 'http://api.rossko.ru/service/GetOrders',
     'options' => array(
         'connection_timeout' => 1,
         'trace' => true
@@ -20,15 +66,28 @@ $connect = array(
 );
 
 $param = array(
-    'KEY1' => '27790',
-    'KEY2' => '27790',
-    'TEXT' => '333114'
+    'KEY1' => 'bb863005b6468e8ceffec6febeee4c84',
+    'KEY2' => '614ef5b7b2bca40dd231c4dc2c79283d',
+    'ORDER_IDS' => array(
+         array(
+            'id' => 1234567
+        ),
+         array(
+            'id' => 1234568
+        )
+    )
 );
 
 $query  = new SoapClient($connect['wsdl'], $connect['options']);
-$result = $query->GetSearch($param);
+$result = $query->GetOrders($param);
 
 print_r($result);
+
+
+
+
+
+
 
 
 ?>
