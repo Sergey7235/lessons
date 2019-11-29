44d606645ae7fed71ad09e6b15d5f95ee83e93f9
= <=>
2019-11-29 20:29:49 +0500

    regular_expression

--- Regular_expression/1_1.php
+++ Regular_expression/1_1.php
@@ -59,11 +59,64 @@
 
 
 
+//                             На {}
+
+// $res = preg_replace('#ab{2,4}a#', '!', 'aa aba abba abbba abbbba abbbbba');
+//  echo $res;
+
 // $res = preg_replace('#ab{0,3}a#', '!', 'aa aba abba abbba abbbba abbbbba');
 //  echo $res;
 
-$res = preg_replace('#ab{4,}a#', '!', 'aa aba abba abbba abbbba abbbbba');
- echo $res;
+// $res = preg_replace('#ab{4,}a#', '!', 'aa aba abba abbba abbbba abbbbba');
+//  echo $res;
+
+
+
+
+//                      На \s, \S, \w, \W, \d, \D
+
+// $res = preg_replace('#(a\da)#', '!', 'a1a a2a a3a a4a a5a aba aca');
+//   echo $res;
+
+// $res = preg_replace('#(a\d{0,}a)#', '!', 'a1a a22a a333a a4444a a55555a aba aca');
+//   echo $res;
+
+// $res = preg_replace('#(a\Db)#', '!', 'avb a1b a2b a3b a4b a5b abb acb');
+//   echo $res;
+
+ // $res = preg_replace('#(a(\W)b)#', '!', 'ave a#b a2b a$b a4b a5b a-b acb');  
+ //   echo $res;
+
+// $res = preg_replace('#\s#', '!', 'ave a#a a2a a$a a4a a5a a-a aca');
+//    echo $res;
+
+
+
+
+//                       На [], '^' - не, [a-zA-Z], кириллицу
+
+
+// $res = preg_replace('#a[bex]a#', '!', 'aba aea aca aza axa');
+//    echo $res;
+
+ // $res = preg_replace('#a[b\.\+\*]a#', '!', 'aba aea aca aza axa a.a a+a a*a');
+ //    echo $res;
+
+ // $res = preg_replace('#a[3-7]a#', '!', 'aba aea aca a5a axa a.a a+a a*a');
+ //    echo $res;
+
+ // $res = preg_replace('#a[a-g]a#', '!', 'aba aea aca a5a axa a.a a+a a*a');
+ //    echo $res;
+
+ // $res = preg_replace('#a[a-fj-s]a#', '!', 'aba aea aca a5a axa aja aka a*a');
+ //    echo $res;
+
+ // $res = preg_replace('#a[a-fA-Z]a#', '!', 'aba aea aca a5a axa aAa aKa aLa');
+ //    echo $res;
+
+ $res = preg_replace('#a[^e]a#', '!', 'aba aea aca aza axa a-a a#a');
+    echo $res;
+
 
 
  ?>
