a613bd32f48c3b2150c03673c4bf93c6e90f1081
Sergey7235 <yshkoff-serg@yandex.com>
2020-02-11 08:06:33 +0500

    Test Job

---
+++ Test_job/templates/index.php
@@ -0,0 +1,77 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	<!-- <meta http-equiv="X-UA-Compatible" content="IE=Edge"> -->
+	<title>Test job</title>
+	<link rel="stylesheet" href="../css/styles.css" type="text/css">
+	<!-- <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Europe:400,300" type="text/css"> -->
+</head>
+<body>
+	<div id="wrapper">
+		<header>
+			<a href="/"><img src="../images/logo.png" alt="World Bank Publications"></a>		
+			<a href="/"><img src="../images/tel_header.png" alt="8-800-100-505" align="right" vspace="25"></a>
+		</header>
+
+		<nav>
+			<!-- <ul class="top-menu">
+				<li><a href="/home/">Кредитные карты</a></li>
+				777
+				<li class="active" href="/contributions/">Вклады</li>
+				<li><a href="/services/">Дебетовая карта</a></li>
+				<li><a href="/partners/">Страхование</a></li>
+				<li><a href="/customers/">Друзья</a></li>
+				<li><a href="/projects/">Интернет-банк</a></li>
+			</ul> -->
+			<div class="topnav">
+			  <a href="/credit_cards/">Кредитные карты</a>
+			  <a class="active" href="/contributions/">Вклады</a>
+			  <a href="/debit_cards/">Дебетовая карта</a>
+			  <a href="/insurance/">Страхование</a>
+			  <a href="/friends/">Друзья</a>
+			  <a href="/internet_bank/">Интернет-банк</a>
+			</div>
+		</nav>
+		<div id="heading">
+			<!-- хлебные крошки -->
+
+			<!-- <ul class="top-menu">
+				<li><a href="/credit_cards/">Кредитные карты</a></li>
+				<li><a class="active" href="/contributions/">Вклады</a></li>
+				<li><a href="/services/">Дебетовая карта</a></li>
+				<li><a href="/partners/">Страхование</a></li>
+				<li><a href="/customers/">Друзья</a></li>
+				<li><a href="/projects/">Интернет-банк</a></li>
+			</ul> -->
+
+			<!-- <ul id="breadcrumbs-one">
+				<li><a href="">Крошки1</a></li>
+				<li><a href="">Крошки2</a></li>
+				<li><a href="">Крошки3</a></li>
+				<li><a href="">Крошки4</a></li>
+				<li><a href="" class="current">Крошки5</a></li>
+ 			</ul> -->
+
+		</div>
+		
+		<footer>
+			<div id="footer">
+					<div>
+						<ul class="down-menu">
+	 						<li><a href="/credit_cards/">Кредитные карты</a></li>
+							<li><a href="/contributions/">Вклады</a></li>
+							<li><a href="/debit_cards/">Дебетовая карта</a></li>
+							<li><a href="/insurance/">Страхование</a></li>
+							<li><a href="/friends/">Друзья</a></li>
+							<li><a href="/internet_bank/">Интернет-банк</a></li>
+						</ul>
+					</div>
+			</div>
+		</footer>
+</div>
+</body>
+</html>
---
+++ Test_job/templates/temp_calc.php
@@ -0,0 +1,103 @@
+<?php 
+
+require_once '../func/calc.php';
+
+ ?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<title>Whitesquare</title>
+	<link rel="stylesheet" href="css/styles.css" type="text/css">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Europe:400,300" type="text/css">
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<div id="wrapper">
+		<header>
+			
+			<a href="/"><img src="images/logo.png" alt="World Bank Publications"></a>
+						
+			<a href="/"><img src="images/tel_header.png" alt="8-800-100-505" align="right" vspace="25"></a>
+			
+		</header>
+
+		<nav>
+			<!-- <ul class="top-menu">
+				<li><a href="/home/">Кредитные карты</a></li>
+				777
+				<li class="active" href="/contributions/">Вклады</li>
+				<li><a href="/services/">Дебетовая карта</a></li>
+				<li><a href="/partners/">Страхование</a></li>
+				<li><a href="/customers/">Друзья</a></li>
+				<li><a href="/projects/">Интернет-банк</a></li>
+			</ul> -->
+
+<div class="topnav">
+  <a href="/credit_cards/">Кредитные карты</a>
+  <a class="active" href="/contributions/">Вклады</a>
+  <a href="/services/">Дебетовая карта</a>
+  <a href="/partners/">Страхование</a>
+  <a href="/customers/">Друзья</a>
+  <a href="/projects/">Интернет-банк</a>
+</div>
+
+
+
+
+		</nav>
+		<div id="heading">
+
+			<!-- хлебные крошки -->
+
+			<!-- <ul class="top-menu">
+				<li><a href="/credit_cards/">Кредитные карты</a></li>
+				<li><a class="active" href="/contributions/">Вклады</a></li>
+				<li><a href="/services/">Дебетовая карта</a></li>
+				<li><a href="/partners/">Страхование</a></li>
+				<li><a href="/customers/">Друзья</a></li>
+				<li><a href="/projects/">Интернет-банк</a></li>
+			</ul> -->
+
+
+
+
+
+
+		
+
+			<!-- <ul id="breadcrumbs-one">
+				<li><a href="">Крошки1</a></li>
+				<li><a href="">Крошки2</a></li>
+				<li><a href="">Крошки3</a></li>
+				<li><a href="">Крошки4</a></li>
+				<li><a href="" class="current">Крошки5</a></li>
+ 			</ul> -->
+
+
+
+		</div>
+		
+	<footer>
+		<div id="footer">
+				<div>
+					<ul class="down-menu">
+						<li><a href="/home/">Кредитные карты</a></li>
+						<li><a href="/about/">Вклады</a></li>
+						<li><a href="/services/">Дебетовая карта</a></li>
+						<li><a href="/partners/">Страхование</a></li>
+						<li><a href="/customers/">Друзья</a></li>
+						<li><a href="/contact/">Интернет-банк</a></li>
+					</ul>
+				</div>
+		</div>
+	</footer>
+</div>
+</body>
+</html>
